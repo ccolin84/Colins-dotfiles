c2a70d4611abce24ce1a753957dc7bb148a12dea
ccolin84 <ccolin84@gmail.com>
2019-01-02 08:33:19 -0500

    update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,31 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(display-line-numbers (quote visual))
+ '(package-selected-packages
+   (quote
+    (elixir-mode scala-mode flycheck cider company magit paredit clojure-mode projectile better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
+
 (require 'ido)
 (ido-mode t)
 
@@ -16,8 +44,37 @@
                       magit
                       company
                       scala-mode
+		      elixir-mode
                       flycheck))
 
 (dolist (p my-packages)
   (unless (package-installed-p p)
     (package-install p)))
+
+(load (expand-file-name "~/.roswell/lisp/quicklisp/slime-helper.el"))
+; (load (expand-file-name "~/quicklisp/slime-helper.el"))
+
+(setf slime-lisp-implementations
+       `((sbcl ("ros" "-Q" "-l" "~/.sbclrc" "-L" "sbcl" "run"))
+         (ccl  ("ros" "-Q" "-l" "~/.ccl-init.lisp" "-L" "ccl-bin" "run"))))
+(setf slime-default-lisp 'sbcl)
+(setq slime-net-coding-system 'utf-8-unix)
+
+(defun lisp-hook-fn ()
+  (interactive)
+  ;; Start slime mode
+  (slime-mode)
+  ;; Some useful key-bindings
+  (local-set-key [tab] 'slime-complete-symbol)
+  (local-set-key (kbd "M-q") 'slime-reindent-defun)
+  ;; We set the indent function. common-lisp-indent-function 
+  ;; will indent our code the right way
+  (set (make-local-variable lisp-indent-function) 'common-lisp-indent-function)
+  ;; We tell slime to not load failed compiled code
+  (setq slime-load-failed-fasl 'never))
+
+;; Finally we tell lisp-mode to run our function on startup
+(add-hook 'lisp-mode-hook 'lisp-hook-fn)
+
+;; (load (expand-file-name "~/.roswell/helper.el"))
+;; (setq inferior-lisp-program "ros -Q run")
