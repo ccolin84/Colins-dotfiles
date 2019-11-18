69f96f233d7ba149070a4f66813e709d318d3750
colinlcrawford <ccolin84@gmail.com>
2019-11-17 20:46:42 -0500

    add paredit mode for slime repl

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -135,6 +135,16 @@
 ;; -----------------------------------------
 
 ;; activate paredit mode
+
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+      (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
+
 (add-hook 'lisp-mode-hook 'paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 (add-hook 'clojure-mode-hook 'paredit-mode)
